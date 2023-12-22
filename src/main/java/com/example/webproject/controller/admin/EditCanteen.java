af8c68b5612ca3a2834692fb3f38c9a5d6fa18af
Cecilia384 <h2948461xsw23@163.com>
2023-12-23 01:11:34 +0800

    按照菜系显示菜品3

--- src/main/java/com/example/webproject/controller/admin/EditCanteen.java
+++ src/main/java/com/example/webproject/controller/admin/EditCanteen.java
@@ -1,30 +1,30 @@
-//package com.example.webproject.controller.admin;
-//
-//import com.example.webproject.model.Canteen;
-//import com.example.webproject.model.User;
-//import com.example.webproject.service.Xu.Admin.AdminActionImpl;
-//import jakarta.servlet.*;
-//import jakarta.servlet.http.*;
-//import jakarta.servlet.annotation.*;
-//
-//import java.io.IOException;
-//
-//@WebServlet(name = "EditCanteen", value = "/EditCanteen")
-//public class EditCanteen extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        Canteen canteen;
-//        int canteenID = Integer.parseInt(request.getParameter("id"));
-//        AdminActionImpl adminAction = new AdminActionImpl();
-//        canteen = adminAction.getCanteen(canteenID);
-//        User manager = adminAction.getAccount(canteen.getManagerID());
-//        request.setAttribute("canteen",canteen);
-//        request.setAttribute("manager",manager);
-//        request.getRequestDispatcher("edit_canteen.jsp").forward(request,response);
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//    }
-//}
+package com.example.webproject.controller.admin;
+
+import com.example.webproject.model.Canteen;
+import com.example.webproject.model.User;
+import com.example.webproject.service.Xu.Admin.AdminActionImpl;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "EditCanteen", value = "/EditCanteen")
+public class EditCanteen extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Canteen canteen;
+        int canteenID = Integer.parseInt(request.getParameter("id"));
+        AdminActionImpl adminAction = new AdminActionImpl();
+        canteen = adminAction.getCanteen(canteenID);
+        User manager = adminAction.getAccount(canteen.getManagerID());
+        request.setAttribute("canteen",canteen);
+        request.setAttribute("manager",manager);
+        request.getRequestDispatcher("edit_canteen.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- src/main/java/com/example/webproject/controller/admin/UpdateCanteen.java
+++ src/main/java/com/example/webproject/controller/admin/UpdateCanteen.java
@@ -1,33 +1,33 @@
-//package com.example.webproject.controller.admin;
-//
-//import com.example.webproject.model.Canteen;
-//import com.example.webproject.service.Xu.Admin.AdminActionImpl;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//
-//import java.io.IOException;
-//
-//@WebServlet(name = "UpdateCanteen", value = "/UpdateCanteen")
-//public class UpdateCanteen extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-//
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        int id = Integer.parseInt(request.getParameter("canteenID"));
-//        String canteenName = request.getParameter("canteenName");
-//        String location = request.getParameter("location");
-//        String opentime = request.getParameter("opentime");
-//        String managerName = request.getParameter("managerName");
-//        String notice = request.getParameter("notice");
-//        AdminActionImpl adminAction = new AdminActionImpl();
-//        Canteen newcanteen = adminAction.canteenEncapsulation(id,canteenName,location,opentime,managerName,notice);
-//        adminAction.editCanteen(newcanteen);
-//        request.getRequestDispatcher("edit_canteen.jsp").forward(request,response);
-//    }
-//}
+package com.example.webproject.controller.admin;
+
+import com.example.webproject.model.Canteen;
+import com.example.webproject.service.Xu.Admin.AdminActionImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "UpdateCanteen", value = "/UpdateCanteen")
+public class UpdateCanteen extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("canteenID"));
+        String canteenName = request.getParameter("canteenName");
+        String location = request.getParameter("location");
+        String opentime = request.getParameter("opentime");
+        String managerName = request.getParameter("managerName");
+        String notice = request.getParameter("notice");
+        AdminActionImpl adminAction = new AdminActionImpl();
+        Canteen newcanteen = adminAction.canteenEncapsulation(id,canteenName,location,opentime,managerName,notice);
+        adminAction.editCanteen(newcanteen);
+        request.getRequestDispatcher("edit_canteen.jsp").forward(request,response);
+    }
+}
