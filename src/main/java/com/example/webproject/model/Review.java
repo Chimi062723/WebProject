1cb22600dd71e35909e06a6f6e94b5ba440f8e23
chimi <chimi9391@outlook.com>
2023-12-22 22:06:36 +0800

    管理员食堂管理

--- src/main/java/com/example/webproject/model/Review.java
+++ src/main/java/com/example/webproject/model/Review.java
@@ -14,4 +14,52 @@ public class Review {
         this.userID = userID;
 
     }
+
+    public int getReviewID() {
+        return reviewID;
+    }
+
+    public void setReviewID(int reviewID) {
+        this.reviewID = reviewID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getDishID() {
+        return dishID;
+    }
+
+    public void setDishID(int dishID) {
+        this.dishID = dishID;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getStatue() {
+        return statue;
+    }
+
+    public void setStatue(int statue) {
+        this.statue = statue;
+    }
 }
