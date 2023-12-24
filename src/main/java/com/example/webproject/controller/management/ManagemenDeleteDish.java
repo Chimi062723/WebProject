31344a880d086c3dce29b4e3a08f9f8dc7fc18ce
Sugarwu <github 1504158451@qq.com>
2023-12-25 02:23:30 +0800

    食堂管理员 删除 菜品信息 dishID 必须提交

--- src/main/java/com/example/webproject/controller/management/ManagemenDeleteDish.java
+++ src/main/java/com/example/webproject/controller/management/ManagemenDeleteDish.java
@@ -1,4 +1,38 @@
 package com.example.webproject.controller.management;
 
-public class ManagemenDeleteDish {
+import com.example.webproject.model.Dish;
+import com.example.webproject.service.Wu.management.ManagementsImpl;
+import com.example.webproject.util.DBhelper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(name = "ManagementUpdateDish", value = "/ManagementUpdateDish")
+public class ManagemenDeleteDish extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        DBhelper db = new DBhelper();
+        db.init();
+        ManagementsImpl managements=new ManagementsImpl();
+        String dishID = request.getParameter("dishID");//菜品id
+        Dish dish=managements.getDish(Integer.parseInt(dishID));
+        int a=managements.deleteDish(dish);
+        Boolean msg=false;
+        if (a>=1) {
+            msg = true;
+        }
+        //成功后 跳转到菜品列表信息
+        request.getRequestDispatcher("ManagementGetDishs?msg="+msg).forward(request,response);
+    }
 }
