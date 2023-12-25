45c67dab80a5329bba3b1458edcabba620734f94
Sugarwu <github 1504158451@qq.com>
2023-12-25 16:02:17 +0800

    食堂管理员 增加 菜品信息

--- src/main/java/com/example/webproject/controller/management/ManagementAddDish.java
+++ src/main/java/com/example/webproject/controller/management/ManagementAddDish.java
@@ -1,4 +1,52 @@
 package com.example.webproject.controller.management;
 
-public class ManagementAddDish {
+import com.example.webproject.model.Dish;
+import com.example.webproject.service.Wu.management.ManagementsImpl;
+import com.example.webproject.util.DBhelper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ManagementAddDish", value = "/ManagementAddDish")
+public class ManagementAddDish extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
+        DBhelper db = new DBhelper();
+        db.init();
+        int userid = db.getUserID(db.dbconn, username); // 当前用户的id
+        ManagementsImpl managements=new ManagementsImpl();
+        int canteenID= managements.getCanteen(userid).getCanteenID();
+        String name = request.getParameter("name");
+        String type = request.getParameter("type");
+        String price = request.getParameter("price");
+        String image = request.getParameter("image");
+        String promotionPrice = request.getParameter("promotionPrice");
+        Dish dish=new Dish();
+        dish.setCanteenID(canteenID);
+        dish.setImage(image);
+        dish.setName(name);
+        dish.setPrice(Double.parseDouble(price));
+        dish.setType(type);
+        dish.setPromotionPrice(Double.parseDouble(promotionPrice));
+        int a=managements.addDish(dish);
+        Boolean msg = false;
+        if (a >= 1) {
+            msg = true;
+        }
+        //成功后 跳转到菜品列表信息   msg 为真 代表成功 假为失败
+        request.getRequestDispatcher("ManagementGetDishs?msg=" + msg).forward(request,response);
+    }
 }
