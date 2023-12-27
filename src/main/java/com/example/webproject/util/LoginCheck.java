0cf8562fb6b0b70fa6548ae5e2f7653123b9369b
Cecilia384 <h2948461xsw23@163.com>
2023-12-27 17:11:07 +0800

    添加提示信息2

--- src/main/java/com/example/webproject/util/LoginCheck.java
+++ src/main/java/com/example/webproject/util/LoginCheck.java
@@ -21,7 +21,8 @@ public class LoginCheck implements Filter{
                 "/webweb_war/login.jsp",
                 "/webweb_war/forgot_password.jsp",
                 "/webweb_war/register.jsp",
-                "/webweb_war/"
+                "/webweb_war/",
+                "/webweb_war/login",
         };
         boolean check =check(urls,requestURI);
         if(check){
